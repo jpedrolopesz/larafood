a9e872473dd74b4a18917448042089d8d4d3723d
jpedrolopesz <jplopeszamonelo@hotmail.com>
2022-04-14 13:12:41 -0300

    Commit Test Application 0.0.1

---
+++ app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateProduct;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class ProductController extends Controller
+{
+    private $repository;
+
+    public function __construct(Product $product)
+    {
+        $this->repository = $product;
+
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function index()
+    {
+        $products = $this->repository->latest()->paginate();
+
+         $this->middleware(['can:products']);
+
+        return view('admin.pages.products.index', compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('admin.pages.products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreUpdateProduct  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(StoreUpdateProduct $request)
+    {
+        $data = $request->all();
+
+        $tenant = auth()->user()->tenant;
+
+        if ($request->hasFile('image') && $request->image->isValid()) {
+            $data['image'] = $request->image->store("tenants/{$tenant->uuid}/products");
+        }
+
+        $this->repository->create($data);
+
+        return redirect()->route('products.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\RedirectResponse|\Illuminate\View\View
+     */
+    public function show($id)
+    {
+        if (!$product = $this->repository->find($id)) {
+            return redirect()->back();
+        }
+
+        return view('admin.pages.products.show', compact('product'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\RedirectResponse|\Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        if (!$product = $this->repository->find($id)) {
+            return redirect()->back();
+        }
+
+        return view('admin.pages.products.edit', compact('product'));
+    }
+
+
+    /**
+     * Update register by id
+     *
+     * @param  \App\Http\Requests\StoreUpdateProduct  $request
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(StoreUpdateProduct $request, $id)
+    {
+        if (!$product = $this->repository->find($id)) {
+            return redirect()->back();
+        }
+
+        $data = $request->all();
+
+        $tenant = auth()->user()->tenant;
+
+        if ($request->hasFile('image') && $request->image->isValid()) {
+
+            if (Storage::exists($product->image)) {
+                Storage::delete($product->image);
+            }
+
+            $data['image'] = $request->image->store("tenants/{$tenant->uuid}/products");
+        }
+
+        $product->update($data);
+
+        return redirect()->route('products.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy($id)
+    {
+        if (!$product = $this->repository->find($id)) {
+            return redirect()->back();
+        }
+
+        if (Storage::exists($product->image)) {
+            Storage::delete($product->image);
+        }
+
+        $product->delete();
+
+        return redirect()->route('products.index');
+    }
+
+
+    /**
+     * Search results
+     *
+     * @param  Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function search(Request $request)
+    {
+        $filters = $request->only('filter');
+
+        $products = $this->repository
+            ->where(function($query) use ($request) {
+                if ($request->filter) {
+                    $query->orWhere('description', 'LIKE', "%{$request->filter}%");
+                    $query->orWhere('title', $request->filter);
+                }
+            })
+            ->latest()
+            ->paginate();
+
+        return view('admin.pages.products.index', compact('products', 'filters'));
+    }
+}
