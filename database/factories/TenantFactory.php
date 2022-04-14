cd28ceb6b00b63a4f3b62240c14aa054a8c202d8
jpedrolopesz <jplopeszamonelo@hotmail.com>
2022-04-14 13:24:53 -0300

    Commit Test Application 0.0.2

--- database/factories/TenantFactory.php
+++ database/factories/TenantFactory.php
@@ -9,7 +9,6 @@ use Faker\Generator as Faker;
 $factory->define(Tenant::class, function (Faker $faker) {
     return [
         'plan_id' => factory(Plan::class),
-        'cnpj' => uniqid() . date('YmdHis'),
         'name' => $faker->unique()->name,
         'email' => $faker->unique()->safeEmail,
     ];
